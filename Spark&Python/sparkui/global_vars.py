6e09ef06e4de1f9ee80abfdd07a4d9ab92c3459a
aacosta1985 <131187154+aacosta1985@users.noreply.github.com>
2024-05-13 14:26:25 -0600

    Remove previous file (templates were mostly unedited)

--- Spark&Python/sparkui/global_vars.py
+++
@@ -1,9 +0,0 @@
-settings_dict = {'v1': None, 'namespace': None, 'base_url': None}
-
-def get_global(variable_id):
-    return settings_dict[variable_id]
-
-def set_global(variable_id, variable_value):
-    settings_dict[variable_id] = variable_value
-
-
--- Spark&Python/sparkui/stream_config.py
+++
@@ -1,4 +0,0 @@
-dataloaders_namespaces = []
-streaming_namespaces = []
-cluster_nm = '<EDITED OUT>'
-aws_reg = 'us-west-2'
